83ff6dfc427efca0c72fc1af25ee712ed5efc0a3
badermalshammari <q8.badoori.11@gmail.com>
2025-02-13 23:37:21 +0300

    Functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
+val greetSeaCreatures = {println("Hello, Deep Sea Adventurer!")}
+
+val findPearl: (Int) -> Int = {num1 -> num1 * num1}
+
+val divideTreasure: (Double, Double) -> Double = {total_Treasure, explorers -> total_Treasure / explorers}
+
+fun calculateDepth(depth1: Int, depth2: Int, differenceFunction: (Int, Int) -> Int): Int {
+    return differenceFunction(depth1, depth2)
+}
+val depthDifferenceLambda: (Int, Int) -> Int = { d1, d2 -> d1 - d2 }
+
+fun String.loud(): String = this.uppercase() + "!"
+
+val diveMessage: (String) -> String = { it.loud() }
+
 fun main() {
-    println("Hello World!")
+    greetSeaCreatures()
+    println(findPearl(6))
+    println(divideTreasure(1000.0, 5.0))
+    val result = calculateDepth(1500, 800, depthDifferenceLambda)
+    println(result)
+    println(diveMessage("deep sea"))
 }
